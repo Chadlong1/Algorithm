0d7fb2bcc50d00ec8139122d42d2ea56592e1c85
Hoon <95194851+Chadlong1@users.noreply.github.com>
2024-05-22 16:57:15 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,21 +1,2 @@
+-- 코드를 입력하세요
 SELECT
-    a.ID
-    , a.NAME
-    , a.HOST_ID
-FROM PLACES a 
-JOIN (
-    SELECT
-        HOST_ID
-    FROM PLACES
-    GROUP BY
-        HOST_ID
-    HAVING
-        COUNT(*) >= 2
-    ) b
-ON a.HOST_ID = b.HOST_ID
-ORDER BY 
-    a.ID;
-
-
-
-
